e679bde63713723b93cdf4eb1e8a17e8fd938d50
JoelCohen7 <70781464+JoelCohen7@users.noreply.github.com>
2024-06-23 15:22:57 -0300

    Invariantes Trie y Nodo en lenguaje natural

--- Trie.java
+++ Trie.java
@@ -2,41 +2,22 @@ package aed;
 
 import java.util.ArrayList;
 
-public class Trie<T> { //InvRep (informal): raiz es un NodoTrie<T> o el trie es vacío (o nunca puede ser vacio?). raiz.valor = null. (Después todo lo demás debería estar en el InvRep de la clase NodoTrie<T>)
+public class Trie<T> { 
 
     /*InvRep de Trie:
-    siendo TrieImp la implementación de Trie (¿Hay que poner modulo TrieImp implementa...?
 
-    pred InvRep(t: TrieImp){
-        //El valor de la raiz tiene que ser null
-        t.raiz.valor = null
-        //Lo de si el trie es vacio o no no lo pongo por ahora porque no se si va
-
-        }
+        //t.raiz.valor = null
+        //t.raiz != null o el trie es vacio
     */
     
-    public class NodoTrie<R> { //InvRep (informal): Cada nodo debe o tener cant_hijos > 0 o ser significado. Si un nodo tiene un hijo en la pos 1, el valor de ese nodo hijo debe ser el caracter ASCII correspondiente a la pos 1. |hijos| = 256.  0 <= cant_hijos <= 256. 
-                               //                   cant_hijos = posiciones no nulas del array hijos.
+    public class NodoTrie<R> {
     /*InvRep de NodoTrie:
-    siendo NodoImp la implementación de NodoTrie
 
-    pred InvRep(n: NodoImp){
-        //cant_hijos debe estar entre 0 y 256 y debe ser igual a la cantidad de elementos no nulos en n.hijos
-            0 <= n.cant_hijos <= 256 && cant.hijos = elemsNoNulos(n.hijos)
-        //long de hijos = 256
-            |n.hijos| = 256
-        //Si un nodo tiene un hijo en la pos 1, el valor de ese nodo hijo debe ser el caracter ASCII correspondiente (CREO QUE TA RE MAL)
-            (forall nodo: NodoImp)(nodo in n.hijos ==> posicionEnLista(nodo, n.hijos) = ASCIIaNum(nodo.valor))
-        }
-
-    aux elemsNoNulos(a: ArrayList<T>) : int
-        {res = (sumatoria desde i=0 hasta |a|-1) if a[i] != null then 1}
-
-    aux posicionEnLista(nodo: NodoImp, a: ArrayList<T>) : int
-        { res = (sumatoria desde i=0 hasta |a|-1) if a[i] = nodo then i }
-
-    aux ASCIIaNum(valor: R) : int
-        realmente ni idea como se hace esto.
+    //cant_hijos debe estar entre 0 y 256 y debe ser igual a la cantidad de elementos no nulos en n.hijos.
+    //cant_hijos = posiciones no nulas del array hijos.
+    //|n.hijos| = 256.
+    //Si un nodo tiene un hijo en la pos 1, el valor de ese nodo hijo debe ser el caracter ASCII correspondiente.
+    
     */
         
         public ArrayList<NodoTrie<R>> hijos;
